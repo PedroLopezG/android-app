2b25281b92b0be99d7153c070eb8916dd773d711
Patricia Sanz Escurin <patricia.sanz.escurin@gmail.com>
2022-11-07 20:00:07 +0100

    About me hecho

---
+++ app/src/main/java/com/example/android_app/AboutTheGame.java
@@ -0,0 +1,14 @@
+package com.example.android_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AboutTheGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_the_game);
+    }
+}
--- app/src/main/java/com/example/android_app/Menu.java
+++ app/src/main/java/com/example/android_app/Menu.java
@@ -2,7 +2,9 @@ package com.example.android_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Menu extends AppCompatActivity {
 
@@ -12,4 +14,13 @@ public class Menu extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
     }
 
+    public void onAboutGame(View view) {
+        Intent i = new Intent(Menu.this, AboutTheGame.class);
+        startActivity(i);
+    }
+
+    public void irALogin(View view) {
+        Intent i = new Intent(Menu.this, MainActivity.class);
+        startActivity(i);
+    }
 }
